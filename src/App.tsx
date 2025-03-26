39410d78176a6218d4eea8beb6831a0c0a28e14d
Park Jinoh <70538046+qkrwlsdh@users.noreply.github.com>
2025-03-26 17:48:08 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { useEffect, useRef, useState } from 'react';
 import { NavermapsProvider } from 'react-naver-maps';
 import { Heading1 } from '@/components/Text.tsx';
 import Wrapper from '@/components/Wrapper.tsx';
-import Account from '@/layout/Account/Account.tsx';
 import Container from '@/layout/Container.tsx';
 import FloatingBar from '@/layout/FloatingBar/FloatingBar.tsx';
 import GalleryWrap from '@/layout/Gallery/GalleryWrap.tsx';
-// import Guestbook from '@/layout/Guestbook/Guestbook.tsx';
 import Invitation from '@/layout/Invitation/Invitation.tsx';
 import Location from '@/layout/Location/Location.tsx';
 import Main from '@/layout/Main/Main.tsx';
@@ -54,10 +52,6 @@ function App() {
           <Heading1>오시는 길</Heading1>
           <Location />
         </Wrapper>
-        {/* <Wrapper>
-          <Heading1>신랑 신부에게</Heading1>
-          <Guestbook />
-        </Wrapper> */}
         <FloatingBar isVisible={isVisible} />
       </Container>
     </NavermapsProvider>
